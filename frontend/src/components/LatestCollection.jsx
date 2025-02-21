f9b329c66d81f16c5df4a2e7c8c404c556e0478f
Naveen S Bandara <sandaru.eng@gmail.com>
2025-02-21 09:43:18 +0530

    feat: add bestseller and policy sections

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -10,12 +10,12 @@ const LatestCollection = () => {
 
 useEffect(() => {
     setLatestProducts(products.slice(0,10));
-})
+},[])
 
   return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
-        <Title text1={"LATEST"} text2={"COLLECTION"} />
+        <Title text1={"LATEST "} text2={"COLLECTION"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 ">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et
           libero nec nisl facilisis ultricies.
