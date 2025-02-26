180e5fddd5b861ad9bd750f375712067583adb69
Naveen S Bandara <sandaru.eng@gmail.com>
2025-02-26 17:30:14 +0530

    Make backend folder structre, config mongodb and cloudinary

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+connectDB();
+connectCloudinary();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//api endpoint
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
