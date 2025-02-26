e92b54b8675f9d5964fb9b251c414c3832489ad4
Naveen S Bandara <sandaru.eng@gmail.com>
2025-02-26 23:08:43 +0530

    feat: add backend user registration feature

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRouter.js';
+
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -15,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 
 //api endpoint
+app.use('/api/user' , userRouter);
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
